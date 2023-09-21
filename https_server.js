478419713b514a37abbdd2fdf0deb4f4e3823dbf
Periklis Kostamis <perikliskostamis@gmail.com>
2023-09-21 18:53:33 +0300

    Refactor server

--- https_server.js
+++ https_server.js
@@ -1,22 +1,31 @@
 const fs = require('fs');
 const https = require('https');
-const crypto = require('crypto');
 const swagger = require('./swagger');
 
 // Import the express module
 const express = require("express");
 
 const app = express();
-
-app.use(express.json())
-
-
 const hostname = 'localhost';
 const port = 8080;
 const key = '12345';
 
+// middleware that handles authorization
+const checkAuthorization = (req, res, next) => {
+	if (key !== req.headers["authorization"]) {
+		return res.status(401).json({ "result": "Unauthorized attempt!", "success": false });
+	}
+	next();
+};
+
+// apply json middleware globally
+app.use(express.json())
+
 // set up swagger
 swagger(app, port)
+
+// apply authorization middleware globally
+app.use(checkAuthorization);
 
 https.createServer(
 		// Provide the private and public key to the server by reading each
@@ -31,74 +40,109 @@ https.createServer(
     console.log("Server listening on https://"+hostname+":"+port+"/");
   });
   
-  
+
+/**
+ * @swagger
+ *
+ * /api:
+ *  post:
+ *    tags:
+ *      - API
+ *    summary: Executes a command
+ *    requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *             command:
+ *               type: string
+ *             id:
+ *               type: integer
+ *             data:
+ *               type: string
+ *           required:
+ *             - id
+ *             - command
+ *           example:
+ *             command: write
+ *             id: 1
+ *             data: some data
+ *    responses:
+ *      '200':
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *              example:
+ *                result: Complete!
+ *                success: true
+ *      '400':
+ *        description: bad request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *      '404':
+ *        description: not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *      '401':
+ *        description: not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *              example:
+ *                result: Unauthorized attempt!
+ *                success: false
+ */
   // STATIC API URL
-app.post('/api', express.json(), (req, res) => {
-  
-  // Check if request is json format
-  if (req.is('application/json')) {
-    // Get the number of post parameters passed
-    const body_length = Object.keys(req.body).length;
-    // Check if the sender provided the right key
-    if (key === req.headers["authorization"]) {
-		const command = req.body["command"];
-		// Check if command parameter exists
-		if (typeof command !== 'undefined' && command ) {
-			// Check for command (case insensitive)
-			command_controller(command.toLowerCase(), res, req.body, body_length);					
-		
-		}
-		else {
-			res.status(400).json({ "result": "Command parameter is missing!", "success": false });
-		}
-    
-    }
-    else {
-	  res.status(401).json({ "result": "Unauthorized attempt!", "success": false });
-      
-    }
-    
-  }
-  else {
-	res.status(400).json({ "result": "JSON syntax error!", "success": false });
-  }
-  
-  
+app.post('/api', (req, res) => {
+	const command = req.body["command"];
+	// Check if command parameter exists
+	if (typeof command !== 'undefined' && command) {
+		// Check for command (case insensitive)
+		post_command_controller(command.toLowerCase(), req, res);
+
+	} else {
+		res.status(400).json({ "result": "Command parameter is missing!", "success": false });
+	}
 });
 
 
-  // DYNAMIC API URL
-app.post('/api/:command', express.json(), (req, res) => {
-  
-  // Check if request is json format
-  if (req.is('application/json')) {
-    // Get the number of post parameters passed
-    const body_length = Object.keys(req.body).length;
-    // Check if the sender provided the right key
-    if (key === req.headers["authorization"]) {
-		const command = req.params.command;
-		// Check if command parameter exists
-		if (typeof command !== 'undefined' && command ) {
-			// Check for command (case insensitive)
-			command_controller(command.toLowerCase(), res, req.body, (body_length+1));				
-		
-		}
-		else {
-			res.status(400).json({ "result": "Command parameter is missing!", "success": false });
-		}
-    
-    }
-    else {
-	  res.status(401).json({ "result": "Unauthorized attempt!", "success": false });
-      
-    }
-    
-  }
-  else {
-	res.status(400).json({ "result": "JSON syntax error!", "success": false });
-  }
-  
-  
+  // DYNAMIC API POST URL
+app.post('/api/:command', (req, res) => {
+	post_command_controller(req.params.command.toLowerCase(), req, res);
+});
+
+// DYNAMIC API GET URL
+app.get('/api/:command', (req, res) => {
+	get_command_controller(req.params.command.toLowerCase(), req, res);
 });
 
 // register error handler
@@ -110,20 +154,35 @@ app.use((err, req, res, next) => {
 	});
 });
 
-function command_controller(command, res, req_body, body_length){
+function post_command_controller(command, req, res){
+	const commands = {
+		'write': write
+	};
 
-	switch(command) {
-	  case 'write':
-		write(res, req_body, body_length);
-		break;
-	  case 'read':
-		read(res, req_body, body_length);
-		break;
-	  default:
-		res.status(404).json({ "result": "Command not found!", "success": false });
+	command_controller(commands, command, req, res);
+}
+
+
+function get_command_controller(command, req, res) {
+	const commands = {
+		'read': read
+	};
+
+	command_controller(commands, command, req, res);
+}
+
+function command_controller(commands, command, req, res) {
+	if (!command) {
+		return res.status(400).json({ "result": "Command parameter is missing!", "success": false });
 	}
 
+	const func = commands[command.toLowerCase()];
 
+	if (func) {
+		func(res, req.body);
+	} else {
+		res.status(404).json({ "result": "Command not found!", "success": false });
+	}
 }
 
 
@@ -146,6 +205,8 @@ function command_controller(command, res, req_body, body_length){
  *               type: integer
  *             data:
  *               type: string
+ *           required:
+ *             - id
  *           example:
  *             id: 1
  *             data: some data
@@ -166,25 +227,49 @@ function command_controller(command, res, req_body, body_length){
  *                success: true
  *      '400':
  *        description: bad request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *      '404':
+ *        description: not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *      '401':
+ *        description: not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
  */
-function write(res, parameters, parameters_length) {
-	// Check the number of parameters
-	if (parameters_length != 3) {
-		res.status(400).json({ "result": "Wrong number of parameters!", "success": false });
-		return;
-	}
-	// Check if parameters names...
-
-	res.status(200).json({ "result": "Complete!", "success": true });
+function write(res, body) {
+	res.status(200).json({ "result": "Complete!", "success": true, body });
 }
 
 /**
  * @swagger
  *
  * /api/read:
- *  post:
+ *  get:
  *    tags:
- *      - Reads
+ *      - Read
  *    summary: Reads some data
  *    responses:
  *      '200':
@@ -203,15 +288,38 @@ function write(res, parameters, parameters_length) {
  *                success: true
  *      '400':
  *        description: bad request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *      '404':
+ *        description: not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
+ *      '401':
+ *        description: not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                result:
+ *                  type: string
+ *                success:
+ *                  type: boolean
  */
-function read(res, parameters, parameters_length) {
-	// Check the number of parameters
-	if (parameters_length != 1) {
-		res.status(400).json({ "result": "Wrong number of parameters!", "success": false });
-		return;
-	}
-	// Check if parameters names...
-
+function read(res) {
 	res.status(200).json({ "result": "Complete!", "success": true });
-
 }
