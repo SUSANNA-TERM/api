b58a48a54fc18da06bd5fd4c4d3083f92d2bfc73
ioanziko <53757637+ioanziko@users.noreply.github.com>
2023-11-10 13:08:04 +0200

    Update index.php

--- php_call_api/index.php
+++ php_call_api/index.php
@@ -4,7 +4,7 @@ require_once './api.php';
 
 // POST Example (Change meter_id if you get an error "chaincode response 500, The asset meters with id ... already exists")
 
-$postdata = '{"meter_id":3,"id":"string2","type":"string","metertype_id":0,"type_id":0,"mote":"string","barcode":"string","consumer":"string","provision":"string","lat":"string","lng":"string","address":"string","description":"string","location_id":0,"address_name":"string"}';
+$postdata = '{"meter_id":2,"id":"string2","type":"string","metertype_id":0,"type_id":0,"mote":"string","barcode":"string","consumer":"string","provision":"string","lat":"string","lng":"string","address":"string","description":"string","location_id":0,"address_name":"string"}';
 $command = 'Meters';
 
 $call = rest_call("POST", $command, $postdata);
@@ -19,9 +19,25 @@ else
 
 echo '<br><br>';
 
+
+$postdata = '{"meter_id":2,"id":"string2","type":"NEW_string","metertype_id":0,"type_id":0,"mote":"string","barcode":"string","consumer":"string","provision":"string","lat":"string","lng":"string","address":"string","description":"string","location_id":0,"address_name":"string"}';
+$command = 'Meters/2';
+
+$call = rest_call("PUT", $command, $postdata);
+if ($call) {
+	print_r($call);
+	if ($call->success)
+		echo 'Success';
+	}
+else
+	echo 'Request failed!';
+
+
+echo '<br><br>';
+ 
 // GET Example ( Meters/{id} )
 
-$command = 'Meters/0';
+$command = 'Meters/2';
 
 $call = rest_call("GET", $command);
 if ($call) {
